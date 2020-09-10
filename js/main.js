fa31e5bb697832cc4de030e782ea82f4f6b40cee
santatra22front <santatra22ratsitohaina@gmail.com>
2020-09-10 14:08:01 +0300

    cfp

--- js/main.js
+++
@@ -1,7 +0,0 @@
-let currentURL = window.location.href;
-
-document.querySelectorAll(".menu a").forEach(p => {
-  if(currentURL.indexOf(p.getAttribute("href")) !== -1){
-    p.classList.add("active");
-  }
-})
