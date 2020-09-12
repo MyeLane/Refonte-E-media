3a1819e122dbb068cb07c7ee37b3eddef10c3ddc
santatra22front <santatra22ratsitohaina@gmail.com>
2020-09-12 08:34:36 +0300

    cfpmedia

--- autre_page/cfp.php
+++ autre_page/cfp.php
@@ -20,20 +20,51 @@
                     <div class="col-12 image">
                         <div class="col-6 pt-4 ml-5 content">
                             <h1 class="pl-5">IMAGE</h1>
-                            <div class="col-10">
-                                <p>Conception cinématographique prise de vue - post production
-                                    PAO - DAO - CAO -dessin...
+                            <div class="col-12 pl-4">
+                                <p class="pl-4">Conception cinématographique <br/> prise de vue - post production
+                                    <br/>PAO - DAO - CAO -dessin...
                                 </p>
                             </div>
                         </div>
                     </div>
 
                     <div class="col-12 web">
+                        
+                            <div class="col-6">
+                            
+                            </div>
 
+                            <div class="col-2">
+                            
+                            </div>
+
+                            <div class="col-6 pt-3 float-left content">
+                                <h1 class="pl-5">WEB</h1>
+                                <div class=" pl-5 pr-5 col-12">
+                                    <p class="pl-5 text-center">
+                                        html5 - css3 - <br/>
+                                        javascript - <br/>
+                                        jQuery - php - sql <br/>
+                                        bootstrap <br/>
+                                        initiation cms et framework - design
+                                    </p>
+                                </div>
+                            </div>
+            
                     </div>
 
-                    <div class="col-12 son">
-
+                    <div class="col-12 pl-5 son">
+                        <div class="col-6 pl-5 pt-3 float-left content">
+                            <h1 class="pl-5">SON</h1>
+                            <div class="col-12 pr-5">
+                                <p class="text-center">
+                                    environnement studio <br/>
+                                    prise de son<br/>
+                                    mixage - traitement de son <br/>
+                                    mastering
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
