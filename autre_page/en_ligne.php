276995f8e238af0a0c14106c69f29122ac162091
LEONG Mye Lane Jennifer Kathuicia <45306895+MyeLane@users.noreply.github.com>
2020-09-14 10:09:48 +0300

    master en ligne

--- autre_page/en_ligne.php
+++ autre_page/en_ligne.php
@@ -13,20 +13,189 @@
     <?php
         include_once '../index/nav.php';
     ?>
-    <section class="">
+    <nav class="navbar navbar-default navbar-expand-lg mt-5">
+        <div class="container-fluid">
+            <ul class="nav navbar-nav mx-auto mt-3">
+                <li class="nav-item"><a class="nav-link" href="#licence">LICENCE Bacc +3</a></li>
+                <li class="nav-item ml-5"><a class="nav-link" href="#master">MASTER Bacc +5</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <section class="licence" id="licence">
+
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 tic">
-            <div class="a1"></div>
-            <div class="a2"></div>
-            <div class="a3"></div>
-            <div class="a4"></div>
-            <div class="a5"></div>
-                <img src="../img/Master univ online-08.png" class="img-tic" width="90%" alt="">
+            <div class="col-12 tic-licence">
+                <div class="a1"></div>
+                <div class="a2"></div>
+                <div class="a3"></div>
+                <div class="a4"></div>
+                <div class="a5"></div>
+                <img src="../img/online/Licence univ online-03.png" class="img-tic img-fluid" width="90%" alt="">
+                <div class="row text col-12">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6"></div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <h1 class="tic-titre  pl-5 pr-5 col-12 col-sm-12 col-md-12 col-lg-12">TIC</h1>
+                        <h3 class="sous-titre pl-5 pb-4">Informatique et Télécommunication</h3>
+                        <h5 class="ml-5 mt-4">objectifs</h5>
+                        <p class="ml-5 text-tic mt-2">La mention Informatique et télécommunication a pour objectif de former des spécialistes dans le domaine du génie informatique et des télécommunications. Les diplômés sont capables de mettre à profit leurs connaissances dans les applications des interconnexions de réseaux, des systèmes de télécommunications et de la sécurisation des échanges. Ils possèdent les compétences pour concevoir, intégrer, exploiter et administrer les systèmes informatiques nécessaires au fonctionnement d'une entreprise mais aussi de mieux adapter les fonctionnalités des réseaux locaux émergeants.</p>
+                        <h5 class="ml-5 mt-4">Contenu de la formation</h5>
+                        <p class="ml-5 text-tic mt-2">La formation en nouvelle technologie de l’Informatique et de la Communication compte parmi les matières qu’elle dispense les matières suivantes : Algorithme, Base de la télécommunication analogique, Algèbre, Cryptographie, Circuit séquentiel, Transformation intégrale, Système embarqué, Management, Probabilité Statistique</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 can-licence">
+                <img src="../img/online/Licence univ online-04.png" class="img-can-master img-fluid" width="90%" alt="">
+                <div class="row text col-12">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <h1 class="tic-titre  pl-5 pr-5 col-12 col-sm-12 col-md-12 col-lg-12">CAN</h1>
+                        <h3 class="sous-titre pl-5 pb-4">Communication Audiovisuelle et Numérique</h3>
+                        <h5 class="ml-5 mt-4">objectifs</h5>
+                        <p class="ml-5 text-tic mt-2">La mention Informatique et télécommunication a pour objectif de former des spécialistes dans le domaine du génie informatique et des télécommunications. Les diplômés sont capables de mettre à profit leurs connaissances dans les applications des interconnexions de réseaux, des systèmes de télécommunications et de la sécurisation des échanges. Ils possèdent les compétences pour concevoir, intégrer, exploiter et administrer les systèmes informatiques nécessaires au fonctionnement d'une entreprise mais aussi de mieux adapter les fonctionnalités des réseaux locaux émergeants.</p>
+                        <h5 class="ml-5 mt-4">Contenu de la formation</h5>
+                        <p class="ml-5 text-tic mt-2">La formation en nouvelle technologie de l’Informatique et de la Communication compte parmi les matières qu’elle dispense les matières suivantes : Algorithme, Base de la télécommunication analogique, Algèbre, Cryptographie, Circuit séquentiel, Transformation intégrale, Système embarqué, Management, Probabilité Statistique</p>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 mpj-licence">
+                <img src="../img/online/Licence univ online-05.png" class="img-mpj-master img-fluid" width="90%" alt="">
+                <div class="row text col-12">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6"></div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <h1 class="tic-titre  pl-5 pr-5 col-12 col-sm-12 col-md-12 col-lg-12">MPJ</h1>
+                        <h3 class="sous-titre pl-5 pb-4">Marketing Publicité et Journalisme</h3>
+                        <h5 class="ml-5 mt-4">objectifs</h5>
+                        <p class="ml-5 text-tic mt-2">La mention Informatique et télécommunication a pour objectif de former des spécialistes dans le domaine du génie informatique et des télécommunications. Les diplômés sont capables de mettre à profit leurs connaissances dans les applications des interconnexions de réseaux, des systèmes de télécommunications et de la sécurisation des échanges. Ils possèdent les compétences pour concevoir, intégrer, exploiter et administrer les systèmes informatiques nécessaires au fonctionnement d'une entreprise mais aussi de mieux adapter les fonctionnalités des réseaux locaux émergeants.</p>
+                        <h5 class="ml-5 mt-4">Contenu de la formation</h5>
+                        <p class="ml-5 text-tic mt-2">La formation en nouvelle technologie de l’Informatique et de la Communication compte parmi les matières qu’elle dispense les matières suivantes : Algorithme, Base de la télécommunication analogique, Algèbre, Cryptographie, Circuit séquentiel, Transformation intégrale, Système embarqué, Management, Probabilité Statistique</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 mba-licence">
+                <img src="../img/online/Licence univ online-06.png" class="img-MBA-master img-fluid" width="90%" alt="">
+                <div class="row text col-12">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <h1 class="tic-titre  pl-5 pr-5 col-12 col-sm-12 col-md-12 col-lg-12">MBA SPECIALISE</h1>
+                        <h3 class="sous-titre pl-5 pb-4">MASTER OF BUSINESS ADMINISTRATION</h3>
+                        <h5 class="ml-5 mt-4">objectifs</h5>
+                        <p class="ml-5 text-tic mt-2">La mention Informatique et télécommunication a pour objectif de former des spécialistes dans le domaine du génie informatique et des télécommunications. Les diplômés sont capables de mettre à profit leurs connaissances dans les applications des interconnexions de réseaux, des systèmes de télécommunications et de la sécurisation des échanges. Ils possèdent les compétences pour concevoir, intégrer, exploiter et administrer les systèmes informatiques nécessaires au fonctionnement d'une entreprise mais aussi de mieux adapter les fonctionnalités des réseaux locaux émergeants.</p>
+                        <h5 class="ml-5 mt-4">Contenu de la formation</h5>
+                        <p class="ml-5 text-tic mt-2">La formation en nouvelle technologie de l’Informatique et de la Communication compte parmi les matières qu’elle dispense les matières suivantes : Algorithme, Base de la télécommunication analogique, Algèbre, Cryptographie, Circuit séquentiel, Transformation intégrale, Système embarqué, Management, Probabilité Statistique</p>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 drt-licence">
+                <img src="../img/online/Licence univ online-07.png" class="img-mpj-master img-fluid" width="90%" alt="">
+                <div class="row text col-12">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6"></div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <h1 class="tic-titre  pl-5 pr-5 col-12 col-sm-12 col-md-12 col-lg-12">DROIT</h1>
+                        <h5 class="ml-5 mt-4">objectifs</h5>
+                        <p class="ml-5 text-tic mt-2">La mention Informatique et télécommunication a pour objectif de former des spécialistes dans le domaine du génie informatique et des télécommunications. Les diplômés sont capables de mettre à profit leurs connaissances dans les applications des interconnexions de réseaux, des systèmes de télécommunications et de la sécurisation des échanges. Ils possèdent les compétences pour concevoir, intégrer, exploiter et administrer les systèmes informatiques nécessaires au fonctionnement d'une entreprise mais aussi de mieux adapter les fonctionnalités des réseaux locaux émergeants.</p>
+                        <h5 class="ml-5 mt-4">Contenu de la formation</h5>
+                        <p class="ml-5 text-tic mt-2">La formation en nouvelle technologie de l’Informatique et de la Communication compte parmi les matières qu’elle dispense les matières suivantes : Algorithme, Base de la télécommunication analogique, Algèbre, Cryptographie, Circuit séquentiel, Transformation intégrale, Système embarqué, Management, Probabilité Statistique</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</section>
+
+
+
+
+
+
+
+    <section class="master" id="master">
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 tic-master">
+                <div class="a1"></div>
+                <div class="a2"></div>
+                <div class="a3"></div>
+                <div class="a4"></div>
+                <div class="a5"></div>
+                <img src="../img/Master univ online-08.png" class="img-tic img-fluid" width="90%" alt="">
                 <div class="row text col-12">
                     <div class="col-12 col-sm-12 col-md-6 col-lg-6"></div>
                     <div class="col-12 col-sm-12 col-md-6 col-lg-6">
                         <h1 class="tic-titre  pl-5 pr-5 col-12 col-sm-12 col-md-12 col-lg-12">INFORMATIQUE ET SYSTEME EMBARQUES</h1>
+                        <h5 class="ml-5 mt-4">objectifs</h5>
+                        <p class="ml-5 text-tic mt-2">La mention Informatique et télécommunication a pour objectif de former des spécialistes dans le domaine du génie informatique et des télécommunications. Les diplômés sont capables de mettre à profit leurs connaissances dans les applications des interconnexions de réseaux, des systèmes de télécommunications et de la sécurisation des échanges. Ils possèdent les compétences pour concevoir, intégrer, exploiter et administrer les systèmes informatiques nécessaires au fonctionnement d'une entreprise mais aussi de mieux adapter les fonctionnalités des réseaux locaux émergeants.</p>
+                        <h5 class="ml-5 mt-4">Contenu de la formation</h5>
+                        <p class="ml-5 text-tic mt-2">La formation en nouvelle technologie de l’Informatique et de la Communication compte parmi les matières qu’elle dispense les matières suivantes : Algorithme, Base de la télécommunication analogique, Algèbre, Cryptographie, Circuit séquentiel, Transformation intégrale, Système embarqué, Management, Probabilité Statistique</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 can-master">
+                <img src="../img/online/Master univ online-09.png" class="img-can-master img-fluid" width="90%" alt="">
+                <div class="row text col-12">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <h1 class="tic-titre  pl-5 pr-5 col-12 col-sm-12 col-md-12 col-lg-12">AUDIOVISUEL ET CINEMATIGRAPHIE</h1>
+                        <h5 class="ml-5 mt-4">objectifs</h5>
+                        <p class="ml-5 text-tic mt-2">La mention Informatique et télécommunication a pour objectif de former des spécialistes dans le domaine du génie informatique et des télécommunications. Les diplômés sont capables de mettre à profit leurs connaissances dans les applications des interconnexions de réseaux, des systèmes de télécommunications et de la sécurisation des échanges. Ils possèdent les compétences pour concevoir, intégrer, exploiter et administrer les systèmes informatiques nécessaires au fonctionnement d'une entreprise mais aussi de mieux adapter les fonctionnalités des réseaux locaux émergeants.</p>
+                        <h5 class="ml-5 mt-4">Contenu de la formation</h5>
+                        <p class="ml-5 text-tic mt-2">La formation en nouvelle technologie de l’Informatique et de la Communication compte parmi les matières qu’elle dispense les matières suivantes : Algorithme, Base de la télécommunication analogique, Algèbre, Cryptographie, Circuit séquentiel, Transformation intégrale, Système embarqué, Management, Probabilité Statistique</p>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 mpj-master">
+                <img src="../img/online/Master univ online-10.png" class="img-mpj-master img-fluid" width="90%" alt="">
+                <div class="row text col-12">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6"></div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <h1 class="tic-titre  pl-5 pr-5 col-12 col-sm-12 col-md-12 col-lg-12">MPJ</h1>
+                        <h3 class="sous-titre pl-5 pb-4">Marketing Publicité et Journalisme</h3>
+                        <h5 class="ml-5 mt-4">objectifs</h5>
+                        <p class="ml-5 text-tic mt-2">La mention Informatique et télécommunication a pour objectif de former des spécialistes dans le domaine du génie informatique et des télécommunications. Les diplômés sont capables de mettre à profit leurs connaissances dans les applications des interconnexions de réseaux, des systèmes de télécommunications et de la sécurisation des échanges. Ils possèdent les compétences pour concevoir, intégrer, exploiter et administrer les systèmes informatiques nécessaires au fonctionnement d'une entreprise mais aussi de mieux adapter les fonctionnalités des réseaux locaux émergeants.</p>
+                        <h5 class="ml-5 mt-4">Contenu de la formation</h5>
+                        <p class="ml-5 text-tic mt-2">La formation en nouvelle technologie de l’Informatique et de la Communication compte parmi les matières qu’elle dispense les matières suivantes : Algorithme, Base de la télécommunication analogique, Algèbre, Cryptographie, Circuit séquentiel, Transformation intégrale, Système embarqué, Management, Probabilité Statistique</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 mba-master">
+                <img src="../img/online/Master univ online-11.png" class="img-MBA-master img-fluid" width="90%" alt="">
+                <div class="row text col-12">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <h1 class="tic-titre  pl-5 pr-5 col-12 col-sm-12 col-md-12 col-lg-12">MBA SPECIALISE</h1>
+                        <h3 class="sous-titre pl-5 pb-4">MASTER OF BUSINESS ADMINISTRATION</h3>
+                        <h5 class="ml-5 mt-4">objectifs</h5>
+                        <p class="ml-5 text-tic mt-2">La mention Informatique et télécommunication a pour objectif de former des spécialistes dans le domaine du génie informatique et des télécommunications. Les diplômés sont capables de mettre à profit leurs connaissances dans les applications des interconnexions de réseaux, des systèmes de télécommunications et de la sécurisation des échanges. Ils possèdent les compétences pour concevoir, intégrer, exploiter et administrer les systèmes informatiques nécessaires au fonctionnement d'une entreprise mais aussi de mieux adapter les fonctionnalités des réseaux locaux émergeants.</p>
+                        <h5 class="ml-5 mt-4">Contenu de la formation</h5>
+                        <p class="ml-5 text-tic mt-2">La formation en nouvelle technologie de l’Informatique et de la Communication compte parmi les matières qu’elle dispense les matières suivantes : Algorithme, Base de la télécommunication analogique, Algèbre, Cryptographie, Circuit séquentiel, Transformation intégrale, Système embarqué, Management, Probabilité Statistique</p>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 drt-master">
+                <img src="../img/online/Master univ online-12.png" class="img-mpj-master img-fluid" width="90%" alt="">
+                <div class="row text col-12">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6"></div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <h1 class="tic-titre  pl-5 pr-5 col-12 col-sm-12 col-md-12 col-lg-12">DROIT</h1>
                         <h5 class="ml-5 mt-4">objectifs</h5>
                         <p class="ml-5 text-tic mt-2">La mention Informatique et télécommunication a pour objectif de former des spécialistes dans le domaine du génie informatique et des télécommunications. Les diplômés sont capables de mettre à profit leurs connaissances dans les applications des interconnexions de réseaux, des systèmes de télécommunications et de la sécurisation des échanges. Ils possèdent les compétences pour concevoir, intégrer, exploiter et administrer les systèmes informatiques nécessaires au fonctionnement d'une entreprise mais aussi de mieux adapter les fonctionnalités des réseaux locaux émergeants.</p>
                         <h5 class="ml-5 mt-4">Contenu de la formation</h5>
