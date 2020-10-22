7d99c24e96f1eecee59e827d3f8159c4dc56f55a
santatra22front <santatra22ratsitohaina@gmail.com>
2020-10-22 16:27:54 +0300

    vitrine

---
+++ autre_page/contact.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CONTACT E-MEDIA</title>
+    <?php
+        include_once '../index/head.php';
+    ?>
+    <link rel="stylesheet" href="../css/contact.css">
+</head>
+    <body>
+        <!-- navbar -->
+        <?php
+            include_once '../index/nav.php';
+        ?>
+
+        <div class="container-fluid">
+			<div class="co-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="col d-flex justify-content-center mt-4">
+					<div class="titre_contact">
+						<h3>CONTACTS</h3>
+					</div>
+				</div>
+
+				<div class="row justify-content-center section_contact">
+					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
+						<div class="bloc_email">
+							<div class="col d-flex justify-content-end">
+								<img src="../img/contact/a295bb333f2bf22a43731d4997e30361.png" width="40px" alt="">
+							</div>
+							<h4 class="text-center">SCOLARITE</h4>
+							<h5 class="text-center">+261341451875</h5>
+						</div>
+					</div>
+					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
+						<div class="bloc_email">
+							<div class="col d-flex justify-content-end">
+								<img src="../img/contact/a295bb333f2bf22a43731d4997e30361.png" width="40px" alt="">
+							</div>
+							<h4 class="text-center">EN LIGNE</h4>
+							<h5 class="text-center">+261341177737</h5>
+						</div>
+					</div>
+					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
+						<div class="bloc_email">
+							<div class="col d-flex justify-content-end">
+								<img src="../img/contact/a295bb333f2bf22a43731d4997e30361.png" width="40px" alt="">
+							</div>
+							<h4 class="text-center">CFP</h4>
+							<h5 class="text-center">+261330225086</h5>
+						</div>
+					</div>
+				</div>
+
+				<div class="row mt-5">
+
+					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
+						<div class="bloc_email">
+							<div class="col d-flex justify-content-end">
+								<img src="../img/contact/pngwing.com(1).png" width="40px" alt="">
+							</div>
+							<h4 class="text-center">SCOLARITE</h4>
+							<h5 class="text-center email_scolariter">scolarite.universite.emedia@gmail.com</h5>
+						</div>
+					</div>
+					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
+						<div class="bloc_email">
+							<div class="col d-flex justify-content-end">
+								<img src="../img/contact/pngwing.com(1).png" width="40px" alt="">
+							</div>
+							<h4 class="text-center">LICENCE EN LIGNE
+							</h4>
+							<h5 class="text-center">emediaenligne2020@gmail.com</h5>
+						</div>
+					</div>
+					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
+						<div class="bloc_email">
+							<div class="col d-flex justify-content-end">
+								<img src="../img/contact/pngwing.com(1).png" width="40px" alt="">
+							</div>
+							<h4 class="text-center">MASTER EN LIGNE
+							</h4>
+							<h5 class="text-center">emediaenligne2020@gmail.com</h5>
+						</div>
+					</div>
+					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
+						<div class="bloc_email">
+							<div class="col d-flex justify-content-end">
+								<img src="../img/contact/pngwing.com(1).png" width="40px" alt="">
+							</div>
+							<h4 class="text-center">CFP</h4>
+							<h5 class="text-center">emedia.cfp@gmail.com</h5>
+						</div>
+					</div>
+
+				</div>
+
+
+			</div>
+		</div
+
+        <!-- script js -->
+        <?php
+            include_once '../index/script.php';
+        ?>
+    </body>
+</html>
--- index/nav.php
+++ index/nav.php
@@ -11,7 +11,7 @@
                 <li class="nav-item ml-4"><a class="nav-link button" id="ligne"  href="en_ligne.php">Univérsité online <span class="bottom-solid"></span></a></li>
                 <li class="nav-item ml-4"><a class="nav-link button" id="bac"  href="bac.php">Bac online <span class="bottom-solid"></span></a></li>
                 <li class="nav-item ml-4"><a class="nav-link button" id="groupe"  href="groupe.php">Groupe <span class="bottom-solid"></span></a></li>
-                <li class="nav-item ml-4"><a class="nav-link button" id="contact"  href="#">Contact <span class="bottom-solid"></span></a></li>
+                <li class="nav-item ml-4"><a class="nav-link button" id="contact"  href="contact.php">Contact <span class="bottom-solid"></span></a></li>
             </ul>
             <ul class="nav navbar-nav pull-sm-right">
                 <li class="nav-item nav-seconnecter pr-2" data-tooltip="Se connecter"><a class="nav-link button nav-connection" href="#lien"><i class="mr-2 nav-connection ml-2 fa fa-user"></i> Se connecter</a></li>
