101a011f2871f243b8241672d7a65904b20b845c
santatra22front <santatra22ratsitohaina@gmail.com>
2020-10-23 13:56:20 +0300

    refonte inscription

--- index/head.php
+++ index/head.php
@@ -2,5 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/fontawesome-free-5.8.2-web/css/all.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
 <link rel="stylesheet" href="css/aos.css">
 <link rel="stylesheet" href="css/style.css">
--- index/script.php
+++ index/script.php
@@ -3,4 +3,4 @@
 <script src="js/bootstrap.min.js"></script>
 <script src="js/aos.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-<script src="js/main.js"></script>
+<script src=""></script>
