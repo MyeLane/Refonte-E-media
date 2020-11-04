92cf98f259f46cd457439dd1f8e504b158d136b7
LEONG Mye Lane Jennifer Kathuicia <45306895+MyeLane@users.noreply.github.com>
2020-11-04 09:28:13 +0300

    contact modif

--- autre_page/contact.php
+++ autre_page/contact.php
@@ -26,38 +26,38 @@
 					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
 						<div class="bloc_email">
 							<div class="col d-flex justify-content-end">
-								<img src="image/a295bb333f2bf22a43731d4997e30361.png" width="40px" alt="">
+								<img src="img/contact/a295bb333f2bf22a43731d4997e30361.png" width="40px" alt="">
 							</div>
-							<h4 class="text-center">SCOLARITE</h4>
-							<h5 class="text-center">+261341451875</h5>
+							<h5 class="text-center" style="color :rgb(0, 174, 255);">UNIVERSITE PRESENTIELLE</h5>
+							<h6 class="text-center">+261341451875</h6>
 						</div>
 					</div>
 					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
 						<div class="bloc_email">
 							<div class="col d-flex justify-content-end">
-								<img src="image/a295bb333f2bf22a43731d4997e30361.png" width="40px" alt="">
+								<img src="img/contact/a295bb333f2bf22a43731d4997e30361.png" width="40px" alt="">
 							</div>
-							<h4 class="text-center">LICENCE EN LIGNE </h4>
-							<h5 class="text-center">+261341177737</h5>
+							<h5 class="text-center" style="color :rgb(0, 174, 255);">LICENCE EN LIGNE </h5>
+							<h6 class="text-center">+261341177737 +261345677707</h6>
 						</div>
 					</div>
 	
 					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
 						<div class="bloc_email">
 							<div class="col d-flex justify-content-end">
-								<img src="image/a295bb333f2bf22a43731d4997e30361.png" width="40px" alt="">
+								<img src="img/contact/a295bb333f2bf22a43731d4997e30361.png" width="40px" alt="">
 							</div>
-							<h4 class="text-center">MASTER EN LIGNE </h4>
-							<h5 class="text-center">+261349177707</h5>
+							<h5 class="text-center" style="color :rgb(0, 174, 255);">MASTER EN LIGNE </h5>
+							<h6 class="text-center">+261341177737 +261349177707</h6>
 						</div>
 					</div>
 					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
 						<div class="bloc_email cfp">
 							<div class="col d-flex justify-content-end">
-								<img src="image/a295bb333f2bf22a43731d4997e30361.png" width="40px" alt="">
+								<img src="img/contact/a295bb333f2bf22a43731d4997e30361.png" width="40px" alt="">
 							</div>
-							<h4 class="text-center">CFP</h4>
-							<h5 class="text-center">+261330225086   +261343277707</h5>
+							<h5 class="text-center" style="color :rgb(0, 174, 255);">FORMATION PROFESSIONNELLE</h5>
+							<h6 class="text-center">+261330225086   +261343277707</h6>
 						
 						</div>
 					</div>
@@ -68,39 +68,39 @@
 					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
 						<div class="bloc_email">
 							<div class="col d-flex justify-content-end">
-								<img src="image/pngwing.com(1).png" width="40px" alt="">
+								<img src="img/contact/pngwing.com(1).png" width="40px" alt="">
 							</div>
-							<h4 class="text-center">SCOLARITE</h4>
-							<h5 class="text-center email_scolariter">scolarite.universite.emedia@gmail.com</h5>
+							<h5 class="text-center" style="color :rgb(0, 174, 255);">UNIVERSITE PRESENTIELLE</h5>
+							<h6 class="text-center email_scolariter">scolarite.universite.emedia@gmail.com</h6>
 						</div>
 					</div>
 					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
 						<div class="bloc_email">
 							<div class="col d-flex justify-content-end">
-								<img src="image/pngwing.com(1).png" width="40px" alt="">
+								<img src="img/contact/pngwing.com(1).png" width="40px" alt="">
 							</div>
-							<h4 class="text-center">LICENCE EN LIGNE
-							</h4>
-							<h5 class="text-center">emediaenligne2020@gmail.com</h5>
+							<h5 class="text-center" style="color :rgb(0, 174, 255);">LICENCE EN LIGNE
+							</h5>
+							<h6 class="text-center">emediaenligne2020@gmail.com</h6>
 						</div>
 					</div>
 					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
 						<div class="bloc_email">
 							<div class="col d-flex justify-content-end">
-								<img src="image/pngwing.com(1).png" width="40px" alt="">
+								<img src="img/contact/pngwing.com(1).png" width="40px" alt="">
 							</div>
-							<h4 class="text-center">MASTER EN LIGNE
-							</h4>
-							<h5 class="text-center">masterenligne@gmail.com</h5>
+							<h5 class="text-center" style="color :rgb(0, 174, 255);">MASTER EN LIGNE
+							</h5>
+							<h6 class="text-center">masterenligne.emedia@gmail.com</h6>
 						</div>
 					</div>
 					<div class="col-12 col-sm-12 col-md-3 col-lg-3">
 						<div class="bloc_email">
 							<div class="col d-flex justify-content-end">
-								<img src="image/pngwing.com(1).png" width="40px" alt="">
+								<img src="img/contact/pngwing.com(1).png" width="40px" alt="">
 							</div>
-							<h4 class="text-center">CFP</h4>
-							<h5 class="text-center">emedia.cfp@gmail.com</h5>
+							<h5 class="text-center" style="color :rgb(0, 174, 255);">FORMATION PROFESSIONNELLE</h5>
+							<h6 class="text-center">emedia.cfp@gmail.com</h6>
 						</div>
 					</div>
 
@@ -108,7 +108,7 @@
 
 
 			</div>
-
+		</div>
 
         <!-- script js -->
         <?php
